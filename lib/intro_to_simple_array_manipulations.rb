269108aa580c5c07832cff7f4774242c1461cb69
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-11 22:11:25 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array, string)
+  array.push(string)
+end
+
+def using_push(array, string)
+  array.push(string)
+end
